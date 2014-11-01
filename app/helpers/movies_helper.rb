5d8f256ff96d641904967bc4e5f290bad9070081
Andrews Lartey <enigmageek@outlook.com>
2014-11-01 12:01:30 +0000

    Improve Index Page UI/UX

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,13 @@
 module MoviesHelper
+	def print_stock(stock)
+		if stock == 0
+			content_tag(:span, "Out of stock", class: "out_stock")
+		else
+			content_tag(:span, "In Stock", class: "In_stock")
+		end
+	end
+
+	def print_price(price)
+		number_to_currency price
+	end
 end
